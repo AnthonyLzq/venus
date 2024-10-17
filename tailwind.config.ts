c1d68caa654678643d195060611d8dd6ba8343fa
AnthonyLzq <sluzquinosa@uni.pe>
2024-10-17 13:15:06 -0500

    feat: updated to nativewind v4

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./app/**/*.{js,ts,jsx,tsx}',],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {
       fontFamily: {
